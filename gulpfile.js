77a5286c2cc36eb00e6aaf657958fecf6a5e8976
Dima Vusyk <d.vusyk@gmail.com>
2021-11-11 22:44:22 +0200

    made main screen

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
 // Static server
-gulp.task('server', function() {
+gulp.task('server', function () {
     browserSync.init({
         server: {
             baseDir: "src"
@@ -14,23 +14,23 @@ gulp.task('server', function() {
     });
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename    ({
+gulp.task('styles', function () {
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename({
             prefix: "",
             suffix: ".min",
         }))
-        .pipe(autoprefixer  ({
-        cascade: false
+        .pipe(autoprefixer({
+            cascade: false
         }))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(gulp.dest("src/css"))
         .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+gulp.task('watch', function () {
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"));
     gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
